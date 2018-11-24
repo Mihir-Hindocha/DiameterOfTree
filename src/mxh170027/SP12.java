ba93081139301aef452ba3b923966f5c29d79f2f
Mihir Hindocha <mihirhindocha97@gmail.com>
2018-11-24 16:00:23 -0600

    Initial Project Setup.

---
+++ src/mxh170027/SP12.java
@@ -0,0 +1,13 @@
+package mxh170027;
+
+import rbk.Graph;
+
+public class SP12 {
+
+    /*
+    Assume g is an acyclic, connected graph (tree).
+     */
+    public int diameter(Graph g){
+        return 0;
+    }
+}
