cc5344bf60cd3499957cd50023cd21c113050386
Mihir Hindocha <mihirhindocha97@gmail.com>
2018-11-25 21:07:47 -0600

    Final Project Submitted.

--- src/mxh170027/SP12.java
+++ src/mxh170027/SP12.java
@@ -1,4 +1,16 @@
-package PVT170000;
+/**
+ * @author Mihir Hindocha - mxh170027
+ * @author Prit Thakkar - pvt170000
+ * @version 1.0: 11/25/2018
+ * <p>
+ * Finding Diameter of a tree.
+ * <p>
+ * Implement the algorithm to find the diameter of a tree using the algorithm
+ * discussed in class, that runs BFS twice. Code this algorithm without
+ * modifying Graph.java and BFSOO.java, using them from package rbk.
+ */
+
+package mxh170027;
 
 import rbk.BFSOO;
 import rbk.Graph;
@@ -8,37 +20,56 @@ import java.util.Scanner;
 
 public class SP12 {
 
-    public static int diameter(Graph g){
+    /**
+     * Method created to find the diameter of the graph. It uses the findFarthest method.
+     * @param g input graph
+     * @return diameter of the input graph as int
+     */
+    public static int diameter(Graph g) {
 
-        if(g.size() == 0) return 0;
+        //if graph is empty
+        if (g.size() == 0) return 0;
 
+        /*
+        BFS run on graph from any node. Here node 1.
+         */
         BFSOO b = new BFSOO(g);
         b.bfs(g.getVertex(1));
 
-
+        /*
+        Finding the farthest node.
+         */
         Graph.Vertex farthest = findFarthestNode(g, b);
+        /*
+        Running BFS on farthest node for Diameter.
+         */
         b.bfs(farthest);
 
         return b.getDistance(findFarthestNode(g, b));
     }
 
 
-    private static Graph.Vertex findFarthestNode(Graph g, BFSOO b){
+    /**
+     * Helper method to find the farthest element in the graph.
+     * @param g input graph
+     * @param b BFSOO object: used for source
+     * @return vertex with the maximum distance from source
+     */
+    private static Graph.Vertex findFarthestNode(Graph g, BFSOO b) {
 
         int max = 0;
         int dist;
         Graph.Vertex farthest = b.getSource();
 
-        for(Graph.Vertex u : g){
+        for (Graph.Vertex u : g) {
             dist = b.getDistance(u);
-            if(max < dist){
+            if (max < dist) {
                 max = dist;
                 farthest = u;
             }
         }
         return farthest;
     }
-
 
     public static void main(String[] args) throws Exception {
         //String string = "7 8   1 2 2   1 3 3   2 4 5   3 4 4   4 5 1   5 1 -7 -1 1";
